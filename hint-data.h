mifl/android_device_qcom_common

power: Enable camera preview hint in powerhal
These changes add the powerhal support to detect
camera preview hint and perform power optimization
in cpu, scheduler.

Change-Id: Ibd7bd1c341a86004dc736aba6a634d6206da8564

power/hint-data.h
@@ -32,6 +32,7 @@
 #define DEFAULT_VIDEO_DECODE_HINT_ID    (0x0B00)
 #define DISPLAY_STATE_HINT_ID           (0x0C00)
 #define DISPLAY_STATE_HINT_ID_2         (0x0D00)
+#define CAM_PREVIEW_HINT_ID             (0x0E00)
 
 struct hint_data {
     unsigned long hint_id; /* This is our key. */

DubMoto/bringup_device_motorola_msm8960dt-common

hint-data.h
@@ -34,6 +34,7 @@
 #define DISPLAY_STATE_HINT_ID_2         (0x0D00)
 #define DEFAULT_AUDIO_HINT_ID           (0x0E00)
 #define DEFAULT_PROFILE_HINT_ID         (0x0F00)
+#define CAM_PREVIEW_HINT_ID             (0x1000)
 
 struct hint_data {
     unsigned long hint_id; /* This is our key. */