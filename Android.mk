16ed5095b83fb3768588a4b3fe59f257fd2b9a67
cyril279 <cyril.x9@gmail.com>
2016-04-12 09:30:00 -0400

    power HAL: Isolate 8960

--- Android.mk
+++ Android.mk
@@ -1,12 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-# Hey Mr. Make Author, DIAF PLX
-ifeq ($(TARGET_POWERHAL_VARIANT),qcom)
-USE_ME := true
-endif
-
-ifneq (,$(filter true,$(USE_ME) $(WITH_QC_PERF)))
-
 # HAL module implemenation stored in
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
@@ -14,58 +7,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
-
-ifneq ($(BOARD_POWER_CUSTOM_BOARD_LIB),)
-  LOCAL_WHOLE_STATIC_LIBRARIES += $(BOARD_POWER_CUSTOM_BOARD_LIB)
-else
-
-# Include target-specific files.
-ifeq ($(call is-board-platform-in-list, msm8974), true)
-LOCAL_SRC_FILES += power-8974.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8960), true)
-LOCAL_SRC_FILES += power-8960.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8226), true)
-LOCAL_SRC_FILES += power-8226.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8610), true)
-LOCAL_SRC_FILES += power-8610.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8909), true)
-LOCAL_SRC_FILES += power-8909.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8916), true)
-LOCAL_SRC_FILES += power-8916.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8952), true)
-LOCAL_SRC_FILES += power-8952.c
-endif
-
-ifeq ($(call is-board-platform-in-list, apq8084), true)
-LOCAL_SRC_FILES += power-8084.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8992), true)
-LOCAL_SRC_FILES += power-8992.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8994), true)
-LOCAL_SRC_FILES += power-8994.c
-endif
-
-ifeq ($(call is-board-platform-in-list, msm8996), true)
-LOCAL_SRC_FILES += power-8996.c
-endif
-
-endif  #  End of board specific list
+LOCAL_SRC_FILES := \
+    hint-data.c \
+    list.c \
+    metadata-parser.c \
+    power.c \
+    power-8960.c \
+    utils.c
 
 ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
 LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
@@ -82,12 +30,6 @@ else
   LOCAL_STATIC_LIBRARIES += $(TARGET_POWER_SET_FEATURE_LIB)
 endif
 
-ifneq ($(CM_POWERHAL_EXTENSION),)
-LOCAL_MODULE := power.$(CM_POWERHAL_EXTENSION)
-else
 LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
-endif
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
-
-endif # TARGET_POWERHAL_VARIANT == qcom || WITH_QC_PERF
